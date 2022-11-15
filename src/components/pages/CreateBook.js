70cac5efa0d75663aa95b16f758aef76e388f5ba
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2022-11-15 21:24:52 +0300

    firestore

--- src/components/pages/CreateBook.js
+++ src/components/pages/CreateBook.js
@@ -1,56 +1,90 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux';
-import { createBook } from '../../store/actions/bookActions';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createBook } from "../../store/actions/bookActions";
 
 const CreateBook = (props) => {
-  const [book,setBook] = useState({
-    title:'',
-    description:''
+  const [book, setBook] = useState({
+    title: "",
+    name: "",
+    author: "",
+    mycomment: "",
+    datetime: "",
+    isbn: "",
   });
-  const [description,setDescription] = useState({});
 
   const handleChange = (e) => {
+    let copyBook = { ...book };
 
-    let copyBook = {...book}
-
-    if(e.target.id == 'title') copyBook['title'] = e.target.value;
-    if(e.target.id == 'description')  copyBook['description'] = e.target.value;
+    if (e.target.id === "title") copyBook["title"] = e.target.value;
+    if (e.target.id === "name") copyBook["name"] = e.target.value;
+    if (e.target.id === "author") copyBook["author"] = e.target.value;
+    if (e.target.id === "mycomment") copyBook["mycomment"] = e.target.value;
+    if (e.target.id === "isbn") copyBook["isbn"] = e.target.value;
+    if (e.target.id === "datetime") copyBook["datetime"] = e.target.value;
 
     setBook(copyBook);
-  }
+  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     props.createBook(book);
-  }
+  };
 
   return (
-    <div className='container'>
-      <form onSubmit={handleSubmit} action="" className='white'>
-        <h5 className='grey-text text-darken-3'>Create Book</h5>
+    <div className="container">
+      <form onSubmit={handleSubmit} action="" className="white">
+        <h5 className="grey-text text-darken-3">Create Book</h5>
         <div className="input-field">
           <label htmlFor="title">Title</label>
-          <input type="text" id='title' onChange={(e)=> handleChange(e)} />
+          <input type="text" id="title" onChange={(e) => handleChange(e)} />
         </div>
         <div className="input-field">
-          <label htmlFor="description">Description</label>
-          <textarea className='materialize-textarea' id='description' onChange={handleChange} />
+          <label htmlFor="name">Book Name</label>
+          <input type="text" id="name" onChange={(e) => handleChange(e)} />
         </div>
         <div className="input-field">
-          <button className='btn green lighten-1 z-depth-0'>Save</button>
+          <label htmlFor="author">Author Name</label>
+          <input type="text" id="author" onChange={(e) => handleChange(e)} />
         </div>
-        <p>{book?.title}</p>
-        <p>{book?.description}</p>
+        <div className="input-field">
+          <label htmlFor="mycomment">My Comment</label>
+          <textarea
+            style={{height:'6rem'}}
+            className="materialize-textarea"
+            id="mycomment"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="isbn">ISBN</label>
+          <textarea
+            className="materialize-textarea"
+            id="isbn"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <label htmlFor="datetime">Datetime</label>
+          <textarea
+            className="materialize-textarea"
+            id="datetime"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-field">
+          <button className="btn green lighten-1 z-depth-0">Save</button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     createBook: (book) => dispatch(createBook(book))
-  }
-}
+  };
+};
 
-export default connect(null,mapDispatchToProps)(CreateBook);
+export default connect(null, mapDispatchToProps)(CreateBook);
--- src/store/actions/bookActions.js
+++ src/store/actions/bookActions.js
@@ -1,6 +1,19 @@
 export const createBook = (book) => {
-    return (dispatch,getState,{getFirebase,getFireStore}) => {
-        // async call
-        dispatch({type:'CREATE_BOOK',book:book})
-    }
-}
+  return (dispatch, getState, { getFirebase,getFirestore }) => {
+    // async call
+    // firestore nesnesi türettik.
+    const firestore = getFirestore();
+    // hangi collection a göndereceksek belirledik.
+    firestore
+      .collection("books")
+      .add({
+        ...book,
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_BOOK", book: book });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_BOOK_ERR", err });
+      });
+  };
+};
