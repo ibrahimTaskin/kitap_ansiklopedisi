ca886b4a0e9d204bea01b2e37e27b0e48e8df536
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2022-12-14 17:04:36 +0300

    rounded removed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = (props) => {
   };
 
   return (
-    <nav class="bg-gray-100 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <nav class="bg-gray-100 border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-800">
       <div class="container flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" class="flex items-center">
           <img
