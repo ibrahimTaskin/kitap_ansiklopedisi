d65217ac2c79e5414efc405ed16bb70a8c091edf
ibrahimTaskin <taskin.ibrahim@hotmail.com>
2024-06-05 00:01:06 +0300

    json updated

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,76 +1,64 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = (props) => {
-  const theme = (e) => {
-    document.documentElement.classList.toggle("dark");
-  };
+const theme = () => {
+  document.documentElement.classList.toggle("dark");
+};
 
-  const ToggleButton = () => {
-    return (
-      <div className="ml-3">
-        <button
-          title="Toggle Theme"
-          onClick={theme}
-          class="
-                w-12 
-                h-6 
-                rounded-full 
-                p-1 
-                bg-gray-400 
-                dark:bg-gray-600 
-                relative 
-                transition-colors 
-                duration-500 
-                ease-in
-                focus:outline-none 
-                focus:ring-2 
-                focus:ring-blue-700 
-                dark:focus:ring-yellow-600 
-                focus:border-transparent
-                "
+const ToggleButton = () => (
+  <div className="ml-3">
+    <button
+      title="Toggle Theme"
+      onClick={theme}
+      className="
+        w-12 
+        h-6 
+        rounded-full 
+        p-1 
+        bg-gray-400 
+        dark:bg-gray-600 
+        relative 
+        transition-colors 
+        duration-500 
+        ease-in
+        focus:outline-none 
+        focus:ring-2 
+        focus:ring-blue-700 
+        dark:focus:ring-yellow-600 
+        focus:border-transparent
+      "
+    >
+      <div id="toggle" className="rounded-full w-4 h-4 bg-blue-600 dark:focus:ring-yellow-600  relative ml-0 dark:ml-6 pointer-events-none transition-all duration-300 ease-out"></div>
+    </button>
+  </div>
+);
+
+const Navbar = () => (
+  <nav className="bg-gray-100 border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-800">
+    <div className="container flex flex-wrap items-center justify-between mx-auto">
+      <Link to="/" className="flex items-center">
+        <img
+          src={"/img/KitapLogo.png"}
+          className="h-6 mr-3 sm:h-9"
+          alt="kitap_logo"
+        />
+      </Link>
+      <div className="flex items-center md:order-2">
+        <a
+          href="https://ibrahimtaskin.cf/"
+          target={"_blank"}
+          className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
         >
-          <div id="toggle" class="rounded-full w-4 h-4 bg-blue-600 dark:focus:ring-yellow-600  relative ml-0 dark:ml-6 pointer-events-none transition-all duration-300 ease-out"></div>
-        </button>
-      </div>
-    );
-  };
-
-  return (
-    <nav class="bg-gray-100 border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-800">
-      <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <Link to="/" class="flex items-center">
           <img
-            src={"/img/KitapLogo.png"}
-            s
-            class="h-6 mr-3 sm:h-9"
-            alt="kitap_logo"
+            className="w-8 h-8 rounded-full"
+            src={"/img/profil_s.jpg"}
+            alt="ibrahim_taskin"
           />
-        </Link>
-        <div class="flex items-center md:order-2">
-          <a
-            href="https://ibrahimtaskin.cf/"
-            target={"_blank"}
-            class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-          >
-            <img
-              class="w-8 h-8 rounded-full"
-              src={"/img/profil_s.jpg"}
-              alt="ibrahim_taskin"
-            />
-          </a>
-          <ToggleButton />
-        </div>
+        </a>
+        <ToggleButton />
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);
 
-const mapStateToProps = (state) => {
-  return {
-    auth: state.firebase.auth,
-  };
-};
-
-export default connect(mapStateToProps)(Navbar);
+export default Navbar;
